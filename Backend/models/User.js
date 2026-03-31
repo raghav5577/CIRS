7de94f3214accfba43e9346221dc26bea6062697
Anvesh <mr.anveshmishra@gmail.com>
2026-04-01 02:55:19 +0530

    password bug fix

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     password : {
         type : String,
         required : true,
-        unique : true,
+        unique : false,
     },
     role : {
         type : String,
